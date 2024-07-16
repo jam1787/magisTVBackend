656a4eb6108b03358a6b2466bde2da96a8c1a3fc
Joaquín Elía <joaquin.elia@hotmail.com>
2024-07-16 16:24:37 -0300

    Add pg module and update database config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,13 +1,19 @@
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST'),
-      port: env('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', true)
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+      },
     },
+    options: {
+      ssl: env.bool('DATABASE_SSL', true)
+    }
   },
 });
