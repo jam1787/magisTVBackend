cbbe6ca0fe9ccdae752acad8499e13558e9625e4
Joaquín Elía <joaquin.elia@hotmail.com>
2024-07-23 18:13:30 -0300

    Update: database config, local and production

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,17 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
+    connection: {
         client: 'postgres',
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-      },
+        connection: {
+            host: env('DATABASE_HOST'),
+            port: env.int('DATABASE_PORT'),
+            database: env('DATABASE_NAME'),
+            user: env('DATABASE_USERNAME'),
+            password: env('DATABASE_PASSWORD'),
+            schema: env('DATABASE_SCHEMA'),
+            ssl: {
+                rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+            },
+        },
+        debug: false,
     },
-    options: {
-      ssl: env.bool('DATABASE_SSL', true)
-    }
-  },
-});
+})
