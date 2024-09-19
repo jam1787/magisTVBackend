813a1646e47c034d18a64869598d2ac8dced156b
Joaquín Elía <joaquin.elia@hotmail.com>
2024-09-19 20:43:50 -0300

    feat: send email to admin on user registration

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register(/*{ strapi }*/) { },
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -16,5 +16,28 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    // Interceptar el evento de registro de usuario y enviar email de bienvenida
+    strapi.db.lifecycles.subscribe({
+      models: ['plugin::users-permissions.user'],
+      async afterCreate(event) {
+        const { result } = event;
+        const emailService = strapi.plugins['email'].services.email;
+
+        try {
+          await emailService.send({
+            to: process.env.SMTP_DEFAULT_FROM,
+            from: process.env.SMTP_DEFAULT_FROM,
+            subject: '¡Nuevo usuario registrado en MagisTV!',
+            html: `
+              <h1>Se ha detectado un nuevo registro de usuario en tu web de MagisTV.</h1>
+              <h2>El nombre de usuario es: </h2><span>${result.username}</span>
+              <p>Puedes verlo desde el siguiente link: <a href="${process.env.STRAPI_URL}/admin/content-manager/collection-types/plugin::users-permissions.user?page=1&pageSize=10&sort=username:ASC&filters[$and][0][username][$eq]=${result.username}">ir a strapi</a></p>`,
+          });
+        } catch (err) {
+          console.error('Error al enviar correo:', err);
+        }
+      },
+    });
+  },
 };
